53172171b2c055b0a301363cfaa17c5dc0fc0523
sternj20 <jessesternmusic@gmail.com>
2017-05-14 21:42:20 -0700

    added logic to implement attack button

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -31,20 +31,28 @@ Go back to step 2
 
 //global variables
 
+var clickCounter = 1;
+var isCharChosen = 'false';
+
 //dom variables
 
 var characters = $(".characters");
 var activeChars = $("#activeChars");
 var activeDefenders = $("#activeDefenders");
 var enemies = $("#enemiesToAttack");
-var isCharChosen = 'false';
+var attack = $("#attack");
+
+//settings hps and attack points
+
+$(".charHps").attr("data-hps", 120);
+characters.attr("data-attackPts", 30);
+
 // -------------1----------------
 
 
 characters.on('click', function() {
 	//if the character you are clicking on is not already in the enemies or defender section
 	if(!$.contains(enemies[0], this) && isCharChosen === 'false'){
-
 		// it moves it down into the character section
 		enemies.append(characters);
 		// the remaining characters move down into enemies to attack
@@ -60,10 +68,35 @@ characters.on('click', function() {
 		activeDefenders.append(this);
 		// Its background turns to black
 		activeDefenders[0].children[0].style.background='black';
-	}
-
-	
+	}	
 });
 
+// Press attack button and you attack the defender for a certain amount that gets increased each time you attack
+attack.on('click', function() {
+	//defining variables for the text divs with hps of character in defender and chosen character areas
+	var defenderHpsText = activeDefenders.find('.charHps');
+	var chosenCharsText = activeChars.find('.charHps');
+	//defining variables for the data with hps of character in defender and chosen character areas and converting to integer
+	var defenderHps = parseInt(defenderHpsText.attr("data-hps"));
+	var chosenCharHps = parseInt(chosenCharsText.attr("data-hps"));
+	//defining variable for amount defender attacks for
+	var defendersAttack = parseInt(characters.attr('data-attackPts'));
+
+	//if there is an enemy in the defender section 
+	if(activeDefenders[0].children.length > 0){
+		// attack the defender for a certain amount that gets increased each time you attack
+		defenderHps -= 8 * clickCounter;
+		// defender attacks you for a fixed amount each time 
+		chosenCharHps -= defendersAttack;
+		//update hps in dom and data value of hps 
+		chosenCharsText.text(chosenCharHps);
+		chosenCharsText.attr('data-hps', chosenCharHps);
+		//increase the click counter each time you click attack to increase attack amount
+		clickCounter++;
+		//update hps in dom and data vlue of hps
+		defenderHpsText.text(defenderHps);
+		defenderHpsText.attr("data-hps", defenderHps);
+	}
+});
 
 
